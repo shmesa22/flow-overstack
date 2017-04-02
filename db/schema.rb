e927bebb08b61d6f3bea37a72aab80d957409780
shmesa22 <shmesa23@gmail.com>
2017-04-02 15:33:41 -0500

    Crea el modelo User

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170402170211) do
+ActiveRecord::Schema.define(version: 20170402203257) do
 
   create_table "questions", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
