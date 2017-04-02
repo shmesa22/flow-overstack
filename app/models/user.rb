3f7df6c42c2fe13a0a80478285250923e72d41f9
shmesa22 <shmesa23@gmail.com>
2017-04-02 18:38:46 -0500

    Agrega Nombre de usuario a validaciones de User

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,13 +15,38 @@
 #  last_sign_in_at        :datetime
 #  current_sign_in_ip     :string
 #  last_sign_in_ip        :string
+#  username               :string
 #
 
 class User < ApplicationRecord
+  attr_accessor :login
   has_many :questions
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
+
+  validates :username, :presence => true, :uniqueness => { :case_sensitive => false }
 end
