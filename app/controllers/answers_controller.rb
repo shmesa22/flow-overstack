ac552806a21711176f56970fa0817970bc85f5f9
shmesa22 <shmesa23@gmail.com>
2017-04-02 19:40:14 -0500

    Implementa funcionalidad Responder

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,17 @@
+class AnswersController < ApplicationController
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.create(answer_params)
+
+    if @answer.errors.any?
+      render 'questions/show'
+    else
+      redirect_to question
+    end
+  end
+
+  private
+    def answer_params
+      params.require(:answer).permit(:content).merge(user: current_user)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'questions#index'
 
-  resources :questions, only: [:index, :show, :new, :create]
+  devise_for :users
+
+  resources :questions, only: [:index, :show, :new, :create] do
+    resources :answers, only: [:create]
+  end
 end
